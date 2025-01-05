9f9c1d5a51e89d0ea3c2ad0452c55063ced4bf8c
ogita23 <ogita23@mrcompute03.toda.is.i.nagoya-u.ac.jp>
2025-01-05 19:29:01 +0900

    fix

--- mos.js
+++ mos.js
@@ -62,10 +62,12 @@ function start_experiment() {
     /*
         or you can write simply as
     */
-    method_paths.push(wav_dir + "set" + set_num + "./wav/set1/natural.list");
-    method_paths.push(wav_dir + "set" + set_num + "./wav/set1/SiFiGAN.list");
-    method_paths.push(wav_dir + "set" + set_num + "./wav/set1/VAE_SiFiGAN_v1.list");
-    method_paths.push(wav_dir + "set" + set_num + "./wav/set1/VAE_SiFiGAN_v2.list");
+    if (set_num == "1") {
+        method_paths.push(wav_dir + "set" + set_num + "./wav/set1/natural.list");
+        method_paths.push(wav_dir + "set" + set_num + "./wav/set1/SiFiGAN.list");
+        method_paths.push(wav_dir + "set" + set_num + "./wav/set1/VAE_SiFiGAN_v1.list");
+        method_paths.push(wav_dir + "set" + set_num + "./wav/set1/VAE_SiFiGAN_v2.list");
+    }
     /*
         end
     */
